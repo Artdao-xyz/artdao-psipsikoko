7ffe41260f42481b71488a23dd1bd354bc89737f
Pedro Falco <fpfalco@gmail.com>
2024-07-13 18:27:09 -0300

    feat: add image loader

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,8 @@
 // create a store in svelte
 import { writable } from 'svelte/store';
 
-export const zineStore = writable(true);
+export const zineStore = writable(false);
 export const mintingStore = writable(false);
 export const scrapbookStore = writable(false);
 export const artistBioStore = writable(false);
+export const progress = writable(0);
